0b1e43532987df24904975ec6c96574ff762a67b
BagusSJ <siwandanabagus@gmail.com>
2026-02-25 11:24:15 +0700

    add: fix readme & requirement

--- DetectionImplement/detectionByImage.py
+++ DetectionImplement/detectionByImage.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load model hasil training
-model = YOLO('best (1).pt')
+model = YOLO('MASUKKAN_NAMA_MODEL')
 
 # Jalankan prediksi pada gambar
 results = model.predict(
-    source='D:/Back Up Bagus Arya/Folder Bagus/cawu 4/Machine Learning/Model_Detection_Trofes/apple_test',   # pastikan nama file sesuai!
+    source='[MASUKKAN_PATH_FOLDER_YG_BERISI_IMG',   # pastikan nama file sesuai!
     save=True,               # simpan hasil ke folder 'runs/detect/predict'
     show=True,
     conf=0.5,                # confidence threshold (bisa turunin ke 0.25 biar lebih sensitif)
